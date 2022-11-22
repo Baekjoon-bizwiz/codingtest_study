fa2de8d03088272e3726d845e4ce1efce49520b9
Leeharin115 <lovehaha11@gmail.com>
2022-11-23 00:01:31 +0900

    [BOJ] 구구단 / Class1
    https://www.acmicpc.net/problem/2739lem/2557

--- Leeharin/Class1/2557.js
+++ Leeharin/Class1/2557.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split(' ');
 
-console.log('Hello World!');
+for (let i = 1; i <= 9; i++) {
+  console.log(`${parseInt(input)} * ${i} = ${parseInt(input) * i}`);
+}
