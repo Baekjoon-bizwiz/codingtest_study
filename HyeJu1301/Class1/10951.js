982223065a8f937861bb834f5a0a67a79c346766
HyeJu <kng04301@naver.com>
2022-12-28 00:34:38 +0900

    [BOJ] A+B - 4 / Class1
    https://www.acmicpc.net/problem/10951

---
+++ HyeJu1301/Class1/10951.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let input = fs.readFileSync('./HyeJu1301/input.txt').toString().trim().split('\n');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let res = []
+
+for (let i = 0; i < input.length; i++) {
+    res = input[i].split(' ');
+    console.log(Number(res[0]) + Number(res[1]));
+}
